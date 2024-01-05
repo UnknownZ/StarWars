d543b2fbc87a285c094ba517d2ddac96baf73c14
UnknownZ <mdonoso04@gmail.com>
2024-01-04 22:06:29 -0300

    WIP fetch fix

--- src/Pages/CharacterContainer.jsx
+++ src/Pages/CharacterContainer.jsx
@@ -4,62 +4,80 @@ import { Context } from '../context/AppContext'
 import URLS from '../config'
 
 function CharacterContainer() {
-  const {store: { people }, actions: {fetchURL, setCharList }}  = useContext(Context)
+  const { store: { people }, actions: { fetchURL, setCharList } } = useContext(Context)
   const [isLoading, setIsLoading] = useState(true)
+  const [nextPage, setNextPage] = useState("")
+  const [previousPage, setPreviousPage] = useState("")
 
   useEffect(() => {
-    if(people)
-    {
-      if(people.length === 0){
-        fetchCharacters(URLS.URL_Characters)
+    const fetchData = async () => {
+      if (people.length == 0) {
+        await fetchCharacters(URLS.URL_Characters);
+
       }
-      else
-      setIsLoading(false)
-    }
-    else 
-    fetchCharacters(URLS.URL_Characters)
-  }, [])
+      setIsLoading(false);
+    };
+    fetchData();
+    console.log(previousPage)
+    console.log(nextPage)
+  }, []);
 
   const fetchCharacters = async (URL) => {
-    let tempURL = URL
-    let characters = [];
-    let data
-    do {
-        data = await fetchURL(tempURL)
-        data.results.map((character) => {
-            characters.push(character)
-        })
-        tempURL = data.next
-    }
-    while (tempURL)
+    let data = await fetchURL(URL)
+    setCharList(data.results)
+    setNextPage(data.next)
+    setPreviousPage(data.previous)
     setIsLoading(false)
-    setCharList(characters)
-}
+  }
 
-  return(
+  const goToNext = () => {
+      setIsLoading(true)
+      fetchCharacters(nextPage)
+  }
+
+  const goToPrevious = () => {
+      setIsLoading(true)
+      fetchCharacters(previousPage)
+    }
+
+  return (
     <div>
-    {isLoading ? (
-      <div>
-        Loading data...
-      </div>
-    ) : (
-      <>
-      <div className="row g-4">
-        <div className="card-group">
-          {
-            people.map((item) => {
-             return <CharacterCard 
-                key={item.url}
-                name={item.name}
-                hairColor={item.hair_color}
-                eyeColor={item.eye_color}
-              />
-            })
-          }
+      {isLoading ? (
+        <div>
+          Loading data...
         </div>
-      </div>
-    </>
-    )}
+      ) : (
+        <>
+          <div className="row g-4">
+            <div className="card-group">
+              {
+                people.map((item) => {
+                  return <CharacterCard
+                    key={item.url}
+                    name={item.name}
+                    hairColor={item.hair_color}
+                    eyeColor={item.eye_color}
+                  />
+                })
+              }
+              {previousPage ?
+                <button className='btn btn-primary cols-1' onClick={goToNext}>
+                  Previous Page
+                </button>
+                :
+                <></>}
+
+              {nextPage ?
+                <button className='btn btn-primary cols-1' onClick={goToPrevious}>
+                  Next page
+                </button >
+                :
+                <></>
+              }
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
